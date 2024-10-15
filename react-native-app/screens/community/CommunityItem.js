27fbcd9fe88b5dbfcbf351497d282533fccac72d
dlxodnd007 <dlxodnd007@naver.com>
2024-10-15 19:51:15 +0900

    chore : 커뮤니티 상태관리 수정

--- react-native-app/screens/community/CommunityItem.js
+++ react-native-app/screens/community/CommunityItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -8,18 +8,17 @@ import {
   Alert,
   TextInput,
   ScrollView,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
-import { S3_URL } from "../../constants/config"; // S3 URL을 constants에서 가져옵니다.
-import { useAuth } from "../../redux/authState";
-import { posts } from "../../redux/postSlice";
-import { fetchProfile } from "../../redux/profileSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { getApiClient } from "../../redux/apiClient";
-import Modal from "react-native-modal";
-import { BottomSheet } from "react-native-elements";
-import { Swipeable } from "react-native-gesture-handler";
-import defaultProfileImage from "../../assets/icons/default-profile.png";
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { S3_URL } from '../../constants/config'; // S3 URL을 constants에서 가져옵니다.
+import { useAuth } from '../../redux/authState';
+import { posts } from '../../redux/postSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { getApiClient } from '../../redux/apiClient';
+import Modal from 'react-native-modal';
+import { BottomSheet } from 'react-native-elements';
+import { Swipeable } from 'react-native-gesture-handler';
+import defaultProfileImage from '../../assets/icons/default-profile.png';
 
 // 배열로 전달된 createdAt을 Date 객체로 변환하는 함수
 const arrayToDate = (arr) => {
@@ -34,7 +33,7 @@ const timeAgo = (dateArray) => {
   const seconds = Math.floor((now - date) / 1000);
 
   if (seconds < 60) {
-    return "방금 전";
+    return '방금 전';
   } else if (seconds < 3600) {
     return `${Math.floor(seconds / 60)}분 전`;
   } else if (seconds < 86400) {
@@ -47,18 +46,19 @@ const timeAgo = (dateArray) => {
 const CommunityItem = ({ post, onDelete, onEdit }) => {
   const { auth } = useAuth();
   const [imageError, setImageError] = useState(false);
-  const dispatch = useDispatch();
   const [liked, setLiked] = useState(false);
   const [localLikeCount, setLocalLikeCount] = useState(post.likeCount);
   const [isCommentModalVisible, setIsCommentModalVisible] = useState(false);
   const [isOptionsModalVisible, setIsOptionsModalVisible] = useState(false);
   const [comments, setComments] = useState([]);
   const [commentsCount, setCommentsCount] = useState(post.commentsCount || 0);
-  const [newComment, setNewComment] = useState("");
+  const [newComment, setNewComment] = useState('');
   const [isDropdownVisible, setIsDropdownVisible] = useState(false);
-
-  const { profile } = useSelector((state) => state.profile); // 프로필 상태에서 profile 정보 가져옴
-  const [profileImage, setProfileImage] = useState(defaultProfileImage); // 기본 이미지를 초기 값으로 설정
+  const [profilePicture, setProfilePicture] = useState(
+    post.profilePicturePath
+      ? { uri: `${S3_URL}/${post.profilePicturePath}` }
+      : defaultProfileImage
+  ); // 프로필 이미지를 초기 값으로 설정
 
   const apiClient = getApiClient(auth.token);
 
@@ -66,31 +66,21 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
   const imagePath =
     post.imageNames && post.imageNames.length > 0 ? post.imageNames[0] : null;
 
-    useEffect(() => {
-      const fetchLikeStatus = async () => {
-        try {
-          const response = await apiClient.get(
-            `/api/likes/post/${post.id}/member/${auth.memberId}`
-          );
-          setLiked(response.data); // 상태 업데이트
-        } catch (error) {
-          console.error("Error fetching like status:", error);
-        }
-      };
-
-      fetchLikeStatus();
-      fetchComments(); // fetchComments는 post.id와 연관됨
-    }, [post.id, auth.memberId, apiClient]); // dispatch는 제외
-
-
-
-    useEffect(() => {
-      if (profile?.profilePicturePath) {
-        setProfileImage({ uri: `${S3_URL}/${profile.profilePicturePath}` }); // URL 경로로 이미지 불러옴
-      } else {
-        setProfileImage(defaultProfileImage); // 없으면 기본 이미지 설정
+  useEffect(() => {
+    const fetchLikeStatus = async () => {
+      try {
+        const response = await apiClient.get(
+          `/api/likes/post/${post.id}/member/${auth.memberId}`
+        );
+        setLiked(response.data);
+      } catch (error) {
+        console.error('Error fetching like status:', error);
       }
-    }, [profile]); // profile만 의존성에 넣으면 됨
+    };
+    fetchLikeStatus();
+
+    fetchComments();
+  }, [post.id, auth.memberId]);
 
   const fetchComments = async () => {
     try {
@@ -98,7 +88,7 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
       setComments(response.data);
       setCommentsCount(response.data.length);
     } catch (error) {
-      console.error("Error fetching comments:", error);
+      console.error('Error fetching comments:', error);
     }
   };
 
@@ -120,7 +110,7 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
     } catch (error) {
       setLiked(!newLiked);
       setLocalLikeCount((prev) => (!newLiked ? prev + 1 : prev - 1));
-      console.error("Error updating like status:", error);
+      console.error('Error updating like status:', error);
     }
   };
 
@@ -152,9 +142,9 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
         );
         setComments([response.data, ...comments]); // Add new comment to the beginning
         setCommentsCount(commentsCount + 1);
-        setNewComment("");
+        setNewComment('');
       } catch (error) {
-        console.error("Error adding comment:", error);
+        console.error('Error adding comment:', error);
       }
     }
   };
@@ -162,7 +152,7 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
     const scale = dragX.interpolate({
       inputRange: [-100, 0],
       outputRange: [1, 0],
-      extrapolate: "clamp",
+      extrapolate: 'clamp',
     });
     return (
       <TouchableOpacity
@@ -179,7 +169,7 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
       setComments(comments.filter((comment) => comment.id !== commentId));
       setCommentsCount(commentsCount - 1);
     } catch (error) {
-      console.error("Error deleting comment:", error);
+      console.error('Error deleting comment:', error);
     }
   };
 
@@ -190,9 +180,9 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
 
   const handleDeletePress = () => {
     toggleDropdown();
-    Alert.alert("게시글 삭제", "정말로 이 게시글을 삭제하시겠습니까?", [
-      { text: "취소", style: "cancel" },
-      { text: "삭제", onPress: () => onDelete(post.id), style: "destructive" },
+    Alert.alert('게시글 삭제', '정말로 이 게시글을 삭제하시겠습니까?', [
+      { text: '취소', style: 'cancel' },
+      { text: '삭제', onPress: () => onDelete(post.id), style: 'destructive' },
     ]);
   };
 
@@ -250,9 +240,9 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
         <View style={styles.leftActions}>
           <TouchableOpacity style={styles.postAction} onPress={handleLikePress}>
             <Ionicons
-              name={liked ? "heart" : "heart-outline"}
+              name={liked ? 'heart' : 'heart-outline'}
               size={24}
-              color={liked ? "#FF4D4D" : "#666"}
+              color={liked ? '#FF4D4D' : '#666'}
             />
           </TouchableOpacity>
           <TouchableOpacity
@@ -294,43 +284,16 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
         isVisible={isCommentModalVisible}
         onBackdropPress={toggleCommentModal}
         onSwipeComplete={toggleCommentModal}
-        swipeDirection={["down"]}
+        swipeDirection={['down']}
         style={styles.bottomModal}
       >
         <View style={styles.modalContent}>
-          <ScrollView style={styles.commentScrollView}>
-            <View style={styles.commentInputWrapper}>
-              <View style={styles.commentInputContainer}>
-                <Image
-                  source={profileImage}
-                  style={styles.commentInputProfileImage}
-                />
-                <View style={styles.inputWrapper}>
-                  <TextInput
-                    style={styles.input}
-                    placeholder="댓글 작성..."
-                    value={newComment}
-                    onChangeText={setNewComment}
-                  />
-                  <TouchableOpacity
-                    onPress={handleAddComment}
-                    style={styles.addCommentButton}
-                  >
-                    <Ionicons name="arrow-up" size={24} color="white" />
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View style={styles.inputShadow} />
-            </View>
+          <ScrollView>
             {comments.map((comment) => (
               <View key={comment.id} style={styles.comment}>
                 <View style={styles.commentHeader}>
                   <Image
-                    source={
-                      comment.profilePicturePath
-                        ? { uri: `${S3_URL}/${comment.profilePicturePath}` }
-                        : defaultProfileImage
-                    }
+                    source={defaultProfileImage}
                     style={styles.commentProfileImage}
                   />
                   <View style={styles.commentContent}>
@@ -344,7 +307,7 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
                     </View>
                     <Text style={styles.commentText}>{comment.content}</Text>
                   </View>
-                  {auth.nickname === comment.nickname && (
+                  {auth.nickname === comment.nickname && ( // nickname 비교
                     <TouchableOpacity
                       onPress={() => handleDeleteComment(comment.id)}
                       style={styles.deleteCommentButton}
@@ -356,6 +319,26 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
               </View>
             ))}
           </ScrollView>
+          <View style={styles.commentInputContainer}>
+            <Image
+              source={defaultProfileImage}
+              style={styles.commentInputProfileImage}
+            />
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.input}
+                placeholder="댓글 작성..."
+                value={newComment}
+                onChangeText={setNewComment}
+              />
+              <TouchableOpacity
+                onPress={handleAddComment}
+                style={styles.addCommentButton}
+              >
+                <Ionicons name="arrow-up" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </Modal>
     </View>
@@ -363,26 +346,23 @@ const CommunityItem = ({ post, onDelete, onEdit }) => {
 };
 const styles = StyleSheet.create({
   postContainer: {
-    backgroundColor: "#fff",
-    shadowColor: "#000",
+    backgroundColor: '#fff',
+    shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 8,
     elevation: 3,
   },
   postHeader: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     padding: 10,
     zIndex: 10, // Add this line
   },
-  dropdownContainer: {
-    zIndex: 20, // Add this line
-  },
   headerLeft: {
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   profileImage: {
     width: 30,
@@ -391,55 +371,58 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   profileusername: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
+  },
+  dropdownContainer: {
+    zIndex: 20, // Add this line
   },
   dropdown: {
-    position: "absolute",
+    position: 'absolute',
     right: 0,
     top: 25,
     backgroundColor: 'rgba(40, 53, 60, 0.8)',
     zIndex: 30,
-    width: 80,
+    width: 100,
     borderRadius: 8,
   },
   option: {
-    flexDirection: "row",
-    justifyContent: "center", // 가운데 정렬을 위해 추가
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center', // 가운데 정렬을 위해 추가
+    alignItems: 'center',
     paddingHorizontal: 10,
     paddingVertical: 10,
   },
   optionText: {
     fontSize: 12,
-    color: "white",
-    textAlign: "center", // 텍스트 가운데 정렬
+    color: 'white',
+    textAlign: 'center', // 텍스트 가운데 정렬
   },
   postImage: {
-    width: "100%",
+    width: '100%',
     aspectRatio: 1,
   },
   noImageText: {
-    textAlign: "center",
-    color: "gray",
+    textAlign: 'center',
+    color: 'gray',
     marginVertical: 10,
   },
 
   memberId: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
   content: {
     fontSize: 16,
-    color: "#333",
+    color: '#333',
   },
   postActions: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     paddingHorizontal: 12,
     paddingTop: 10,
   },
   leftActions: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   postAction: {
     marginRight: 10,
@@ -449,15 +432,15 @@ const styles = StyleSheet.create({
     paddingBottom: 30,
   },
   likeCount: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginTop: 5,
   },
   contentRow: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginBottom: 5,
   },
   username: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginRight: 5,
     marginTop: 5,
   },
@@ -467,33 +450,33 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   viewComments: {
-    color: "#666",
+    color: '#666',
   },
   actionText: {
     marginLeft: 5,
-    color: "#666",
+    color: '#666',
     fontSize: 14,
   },
   bottomModal: {
-    justifyContent: "flex-end",
+    justifyContent: 'flex-end',
     margin: 0,
   },
   modalContent: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 22,
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    borderColor: "rgba(0, 0, 0, 0.1)",
-    height: "60%",
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    maxHeight: '80%',
   },
   comment: {
     paddingVertical: 10,
     borderBottomWidth: 1,
-    borderBottomColor: "#f0f0f0",
+    borderBottomColor: '#f0f0f0',
   },
   commentHeader: {
-    flexDirection: "row",
-    alignItems: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'flex-start',
   },
   commentProfileImage: {
     width: 40,
@@ -501,30 +484,36 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginRight: 10,
   },
+
   commentUserInfo: {
-    flexDirection: "row",
-    alignItems: "baseline", // 'center' 대신 'baseline'을 사용합니다.
+    flexDirection: 'row',
+    alignItems: 'baseline', // 'center' 대신 'baseline'을 사용합니다.
   },
   commentContent: {
     flex: 1,
   },
   commentCreatedAt: {
+    marginLeft: 10,
     fontSize: 12,
-    marginLeft: 5, // 아이디와 timestamp 사이에 약간의 간격을 줍니다.
-    color: "#999",
+    color: '#999',
+    fontWeight: 'normal',
+    marginLeft: 5,
   },
   commentUsername: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 2,
   },
   commentText: {
     fontSize: 14,
-    color: "#333",
+    color: '#333',
   },
   commentInputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    paddingBottom: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#f0f0f0',
+    paddingTop: 10,
   },
   commentInputProfileImage: {
     width: 40,
@@ -534,10 +523,10 @@ const styles = StyleSheet.create({
   },
   inputWrapper: {
     flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
     borderWidth: 1,
-    borderColor: "#ddd",
+    borderColor: '#ddd',
     borderRadius: 20,
     paddingLeft: 15,
     paddingRight: 5,
@@ -547,30 +536,29 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
   },
   addCommentButton: {
-    alignItems: "center",
     borderRadius: 20,
-    padding: 5,
-    backgroundColor: "#0095f6",
+    padding: 3,
+    backgroundColor: '#0095f6',
   },
 
   deleteCommentButton: {
     padding: 5,
   },
   timeAgo: {
-    color: "#999",
+    color: '#999',
     fontSize: 12,
     marginTop: 5,
   },
 
   cancelButton: {
-    alignItems: "center",
+    alignItems: 'center',
     paddingVertical: 15,
     marginTop: 10,
   },
   cancelButtonText: {
     fontSize: 18,
-    color: "#007AFF",
-    fontWeight: "bold",
+    color: '#007AFF',
+    fontWeight: 'bold',
   },
 });
 
